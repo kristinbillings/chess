51b4e9c92bbc9f54ec45ef8e65db7a36d47a5007
kristinbillings <kris10d@byu.edu>
2025-01-18 23:18:13 -0700

    piece moves working for king byt only when step though and the placement pairs are all the same as expected

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,120 +14,52 @@ public interface PieceMovesCalculator {
         return false;
     }
 
-    /** checks whether the path of the piece is clear or obstructed
-     * this is to see if a movement is valid or not
-     */
-    default public boolean isPathClear(ChessBoard board, ChessPosition myPosition, ChessPosition possibleMove) {
-        boolean isPathClear = true;
-
-        int changeCol = possibleMove.getColumn() - myPosition.getColumn();
-        int changeRow = possibleMove.getRow() - myPosition.getRow();
-
-
-        if (changeCol == 0) {
-            if (changeRow > 0) {
-                for (int j = 1; j < changeRow; j++) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow() + j, myPosition.getColumn())) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            } else {
-                for (int j = -1; j > changeRow; j--) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow() + j, myPosition.getColumn())) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            }
-
-        } else if (changeRow == 0) {
-            if (changeCol > 0) {
-                for (int j = 1; j < changeCol; j++) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow(), myPosition.getColumn() + j)) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            } else {
-                for (int j = -1; j > changeCol; j--) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow(), myPosition.getColumn() + j)) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            }
-        } else {
-            if (changeRow > 0 && changeCol > 0) {
-                for (int j = 1; j < changeRow; j++) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow() + j, myPosition.getColumn() + j)) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            } else if (changeRow > 0 && changeCol < 0) {
-                for (int j = 1; j < changeRow; j++) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow() + j, myPosition.getColumn() - j)) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            } else if (changeRow < 0 && changeCol > 0) {
-                for (int j = 1; j < changeCol; j++) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow() - j, myPosition.getColumn() + j)) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            } else {
-                for (int j = -1; j > changeCol; j--) {
-                    if (board.getPiece(new ChessPosition(myPosition.getRow() + j, myPosition.getColumn() + j)) != null) {
-                        isPathClear = false;
-                        break;
-                    }
-                }
-            }
-        }
-        return isPathClear;
+    default boolean canMoveALot() {
+        return false;
     }
 
+    private boolean checkIfSpaceOccupied(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
+        boolean isValid = false;
+
+        if (position.getRow() >= 0 && position.getRow() <= 7 && position.getColumn() >= 0 && position.getColumn() <= 7) {
+            if (board.getPiece(position) == null) {
+                isValid = true;
+            } else if (color != (board.getPiece(position)).getTeamColor()) {
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
+
+    /**
+     * checks whether the path of the piece is clear or obstructed
+     * this is to see if a movement is valid or not
+     */
 
     default public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> pieceMoves = new java.util.ArrayList<>(List.of());
-                                            //intellij told me to change this^^???
+        Collection<ChessMove> pieceMoves = new ArrayList<>();
+
         int[][] pieceMovements = moveDirections();
 
         for (int i = 0; i < pieceMovements.length; i++) {
-            ChessPosition possibleMove = new ChessPosition(pieceMovements[i][0] + myPosition.getRow(), pieceMovements[i][1] + myPosition.getColumn());
+            ChessPosition endPosition = new ChessPosition(pieceMovements[i][0] + myPosition.getRow(), pieceMovements[i][1] + myPosition.getColumn());
             //checks that the movement is on the board
-            if (possibleMove.getRow() >= 0 && possibleMove.getRow() <= 7 && possibleMove.getColumn() >= 0 && possibleMove.getColumn() <= 7) {
-                //checks to see if the position is empty
-                if (board.getPiece(possibleMove) == null) {
-                    //if the piece can jump (knight), then immediately add position
-                    if (canJump()) {
-                        pieceMoves.add(new ChessMove(myPosition, possibleMove, null));
-                    } else { //checks to see if other pieces in the way of movement
-                        if (isPathClear(board, myPosition, possibleMove)) {
-                            pieceMoves.add(new ChessMove(myPosition, possibleMove, null));
-                        }
-                    }
-                } else { //checks to see if space occupied by other player's piece
-                    ChessGame.TeamColor currentType = (board.getPiece(myPosition)).getTeamColor();
-                    ChessGame.TeamColor typeAtNewLocation = (board.getPiece(possibleMove)).getTeamColor();
+            int bruh = pieceMovements[i][0];
 
-                    if (currentType != typeAtNewLocation) {
-                        if (canJump()) {
-                            pieceMoves.add(new ChessMove(myPosition, possibleMove, null));
-                        } else if (isPathClear(board,myPosition,possibleMove)) {
-                            pieceMoves.add(new ChessMove(myPosition, possibleMove, null));
-                        }
-                    }
+            if (canMoveALot()) {
+
+            } else {
+                //check to see if position can be used
+                ChessGame.TeamColor currentColor = (board.getPiece(myPosition)).getTeamColor();
+                if (checkIfSpaceOccupied(board, endPosition, currentColor)) {
+                    pieceMoves.add(new ChessMove(myPosition, endPosition, null));
                 }
+                //if true add to pieceMoves
+
             }
+
+
         }
         return pieceMoves;
     }
-
-
-
 }
