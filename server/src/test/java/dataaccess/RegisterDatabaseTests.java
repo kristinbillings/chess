60f2f65436521e31cc695ed239433bd2381877f0
kristinbillings <kris10d@byu.edu>
2025-03-12 19:32:28 -0600

    created database test folder

--- server/src/test/java/dataaccess/RegisterDatabaseTests.java
+++ server/src/test/java/dataaccess/RegisterDatabaseTests.java
@@ -1,14 +1,14 @@
-package service;
+package dataaccess;
 
-import dataaccess.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import requests.RegisterRequest;
 import results.RegisterResult;
+import service.UserService;
 
-public class RegisterTests {
+public class RegisterDatabaseTests {
     private UserService userService;
     private MySQLAuthDAO authDAO; //changed from memory to database
     private MySQLUserDAO userDAO; //changed from memory to database
@@ -38,14 +38,6 @@ public class RegisterTests {
         expected = new RegisterResult(username,authToken);
 
         Assertions.assertEquals(expected, actual);
-    }
 
-    @Test
-    public void testInvalidRegistration() throws DataAccessException,ResponseException {
-        RegisterRequest request = new RegisterRequest(null,"urmom","hottie@gmail.com");
-
-        Assertions.assertThrows(dataaccess.ResponseException.class, () -> {
-            userService.register(request);
-        }, "Error: unable to update user");
     }
 }
