2505575ab08dd0281b9f0736af06c4211a5f3135
kristinbillings <kris10d@byu.edu>
2025-03-25 10:45:16 -0600

    started on menus in client

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -3,18 +3,61 @@ package ui;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import static ui.EscapeSequences.*;
+
 import ui.ChessBoard;
+import net.ServerFacade;
 import chess.ChessPiece;
 
 
 public class Client {
     //draws menu and selects input*****
     //then calls methods in chessboard
-    //ima just use the letters as the characters(this is less work)
-    private static void drawChessBoard() {
-        ChessBoard.drawChessBoard("WHITE",new ChessPiece[8][8]); //change this later to be a variable passed in
+    private final String serverUrl;
+    private State state = State.SIGNEDOUT;
+
+    public Client(String serverUrl) {
+        ServerFacade server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
     }
 
 
+
+
+
+    private static void drawChessBoard() {
+        //takes in the board (will be the one that comes from the game
+        ChessBoard.drawChessBoard("WHITE",new ChessPiece[8][8]); //change this later to be a variable passed in
+    }
+
+    public String PreloginMenu() {
+        return """
+                Welcome to chess!
+                                
+                - login         login to play
+                - register      register to begin playing
+                - quit          exits chess program
+                - help          lists options
+                """;
+    }
+
+    public String PostloginMenu() {
+        return """
+                What would you like to do?
+                            
+                - create        create a new game
+                - list          list of all the games currently open
+                - play          join a game and play
+                - observe       watch a game
+                - logout        logout of chess
+                - help          lists options
+                """;
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return PreloginMenu();
+        }
+        return PostloginMenu();
+    }
 
 }
